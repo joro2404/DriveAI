f3b02b4027be9ee9f93a56126f50fc24ed41e51e
joro2404 <joro2404@abv.bg>
2021-01-11 14:42:11 +0200

    minor

--- mask/model.py
+++ mask/model.py
@@ -10,6 +10,9 @@ from sklearn.model_selection import train_test_split
 data=np.load('data.npy')
 target=np.load('target.npy')
 
+show_accuracy = 0
+Show_loss = 0
+
 
 model=Sequential()
 
@@ -36,20 +39,22 @@ train_data,test_data,train_target,test_target=train_test_split(data,target,test_
 checkpoint = ModelCheckpoint(filepath='checkpoints/model-{epoch:03d}.hdf5',monitor='val_loss',verbose=0,save_best_only=True,mode='auto')
 history=model.fit(train_data,train_target,epochs=20,callbacks=[checkpoint],validation_split=0.2)
 
-plt.plot(history.history['loss'],'r',label='training loss')
-plt.plot(history.history['val_loss'],label='validation loss')
-plt.xlabel('# epochs')
-plt.ylabel('loss')
-plt.legend()
-plt.show()
+
+if(Show_loss):
+    plt.plot(history.history['loss'],'r',label='training loss')
+    plt.plot(history.history['val_loss'],label='validation loss')
+    plt.xlabel('# epochs')
+    plt.ylabel('loss')
+    plt.legend()
+    plt.show()
 
 
-
-plt.plot(history.history['accuracy'],'r',label='training accuracy')
-plt.plot(history.history['val_accuracy'],label='validation accuracy')
-plt.xlabel('# epochs')
-plt.ylabel('loss')
-plt.legend()
-plt.show()
+if(show_accuracy):
+    plt.plot(history.history['accuracy'],'r',label='training accuracy')
+    plt.plot(history.history['val_accuracy'],label='validation accuracy')
+    plt.xlabel('# epochs')
+    plt.ylabel('loss')
+    plt.legend()
+    plt.show()
 
 print(model.evaluate(test_data,test_target))
