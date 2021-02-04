db12ce4d8ef57a46d3c1c2434f7ceddd638e869d
joro2404 <joro2404@abv.bg>
2021-02-04 10:58:39 +0200

    plot funcition

--- mask/model.py
+++ mask/model.py
@@ -7,7 +7,27 @@ from tensorflow.keras.layers import Dense, Activation, Flatten, Dropout, Conv2D,
 from tensorflow.keras.callbacks import ModelCheckpoint
 from data import validation_generator, train_generator
 
-
+def plot_model_history(model_history):
+    
+    fig, axs = plt.subplots(1,2,figsize=(15,5))
+    # summarize history for accuracy
+    axs[0].plot(range(1,len(model_history.history['accuracy'])+1),model_history.history['accuracy'])
+    axs[0].plot(range(1,len(model_history.history['val_accuracy'])+1),model_history.history['val_accuracy'])
+    axs[0].set_title('Model Accuracy')
+    axs[0].set_ylabel('Accuracy')
+    axs[0].set_xlabel('Epoch')
+    axs[0].set_xticks(np.arange(1,len(model_history.history['accuracy'])+1),len(model_history.history['accuracy'])/10)
+    axs[0].legend(['train', 'val'], loc='best')
+    # summarize history for loss
+    axs[1].plot(range(1,len(model_history.history['loss'])+1),model_history.history['loss'])
+    axs[1].plot(range(1,len(model_history.history['val_loss'])+1),model_history.history['val_loss'])
+    axs[1].set_title('Model Loss')
+    axs[1].set_ylabel('Loss')
+    axs[1].set_xlabel('Epoch')
+    axs[1].set_xticks(np.arange(1,len(model_history.history['loss'])+1),len(model_history.history['loss'])/10)
+    axs[1].legend(['train', 'val'], loc='best')
+    fig.savefig('plot.png')
+    plt.show()
 
 show_accuracy = 0
 show_loss = 0
@@ -35,7 +55,7 @@ model.compile(loss='binary_crossentropy',optimizer='adam',metrics=['accuracy'])
 
 checkpoint = ModelCheckpoint(filepath='checkpoints/model-{epoch:03d}.hdf5',monitor='val_loss',verbose=0,save_best_only=True,mode='auto')
 history = model.fit_generator(train_generator,epochs=10,validation_data=validation_generator,callbacks=[checkpoint])
-
+plot_model_history(history)
 
 if(show_loss):
     plt.plot(history.history['loss'],'r',label='training loss')
