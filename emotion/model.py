86c24cf58fdf7747ce174eece37a21ac4bce3529
joro2404 <joro2404@abv.bg>
2021-02-07 15:39:10 +0200

    model topology

--- emotion/model.py
+++ emotion/model.py
@@ -3,10 +3,14 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, Activation, Flatten, Dropout, Conv2D,MaxPooling2D
+from tensorflow.keras.layers import Dense, Activation, Flatten, Dropout, Conv2D,MaxPooling2D,BatchNormalization
 from tensorflow.keras.callbacks import ModelCheckpoint
 from data import validation_generator, train_generator
 
+
+num_train = 28709
+num_val = 7178
+num_epoch = 50
 
 def plot_model_history(model_history):
 
@@ -29,3 +33,32 @@ def plot_model_history(model_history):
     axs[1].legend(['train', 'val'], loc='best')
     fig.savefig('plot.png')
     plt.show()
+
+
+model = Sequential()
+
+model.add(Conv2D(32, kernel_size=(3, 3), padding='same', input_shape=(48,48,1)))
+model.add(BatchNormalization())
+model.add(Activation('relu'))
+model.add(Conv2D(64, kernel_size=(3, 3), padding='same'))
+model.add(BatchNormalization())
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2), padding='same'))
+model.add(Dropout(0.5))
+
+model.add(Conv2D(128, kernel_size=(3, 3), padding='same'))
+model.add(BatchNormalization())
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2), padding='same'))
+model.add(Dropout(0.5))
+
+model.add(Conv2D(128, kernel_size=(3, 3), padding='same'))
+model.add(BatchNormalization())
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2), padding='same'))
+model.add(Dropout(0.5))
+
+model.add(Flatten())
+model.add(Dense(1024, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(7, activation='softmax'))
