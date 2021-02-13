96de718e814869f97eaebfd470cf6a784ecdd882
joro2404 <joro2404@abv.bg>
2021-02-13 17:19:02 +0200

    detect files fixed

---
+++ emotion/detect.py
@@ -0,0 +1,36 @@
+import cv2
+import numpy as np
+from tensorflow.keras.models import load_model
+
+model=load_model('./checkpoints/model-046.hdf5')
+
+cv2.ocl.setUseOpenCL(False)
+
+emotion_dict = {0: "Angry", 1: "Disgusted", 2: "Fearful", 3: "Happy", 4: "Neutral", 5: "Sad", 6: "Surprised"}
+
+cap = cv2.VideoCapture(0)
+
+haarcascade = cv2.CascadeClassifier('./haarcascade_frontalface_default.xml')
+
+while True:
+    ret, frame = cap.read()
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = haarcascade.detectMultiScale(gray,scaleFactor=1.3, minNeighbors=5)
+
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y-50), (x+w, y+h+10), (255, 0, 0), 2)
+        roi_gray = gray[y:y + h, x:x + w]
+        cropped_img = np.expand_dims(np.expand_dims(cv2.resize(roi_gray, (48, 48)), -1), 0)
+        prediction = model.predict(cropped_img)
+        print(prediction.tolist())
+        maxindex = int(np.argmax(prediction))
+        cv2.putText(frame, emotion_dict[maxindex], (x+20, y-60), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
+
+    cv2.imshow("Frame", frame)
+    key = cv2.waitKey(1) & 0xFF
+    
+    if key == 27:
+        break
+
+cap.release()
+cv2.destroyAllWindows()
